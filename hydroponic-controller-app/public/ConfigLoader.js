b18ffbec614e9ec3ae9e2238638396ff4fa5b500
Charlemagne Wong <frian13@hotmail.com>
2021-02-20 17:14:21 -0600

    Change how config is loaded to app

---
+++ hydroponic-controller-app/public/ConfigLoader.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+class ConfigLoader
+{
+    constructor(filepath='./Config.json') {
+        this.filepath = filepath;
+    }
+
+    /**
+     * Checks if file exists
+     * Note: I know this is inefficient, but it is only used once
+     */
+    exists() {
+        try {
+            require(this.filepath);
+            return true;
+        }
+        catch (err) {
+            return false;
+        }
+    }
+    
+    load() {
+        if (this.exists()) {
+            return require(this.filepath);
+        }
+
+        return null
+    }
+
+    async write(data, callback=()=>{}) {
+        if (typeof data == 'object') {
+            data = JSON.stringify(data, null, 4);
+        }
+        fs.writeFile(path.join(__dirname, this.filepath), data, callback);
+    }
+}
+
+module.exports = ConfigLoader;
