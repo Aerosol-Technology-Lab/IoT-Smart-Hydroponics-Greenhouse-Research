da025bc500e1b3cf834524e2fd2a94f81a20b8d7
Charlemagne Wong <frian13@hotmail.com>
2021-02-22 09:53:28 -0600

    Add shortcut to enable or disable mouse cursor

--- hydroponic-controller-app/src/components/Navigation.js
+++ hydroponic-controller-app/src/components/Navigation.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Navigation.css';
 import '../style.css'
-class Navigation extends React.Component {
-    render() {
-        return(
-            <div className={"Navigation"}>
-                <span className='icon icon-dashboard'></span>
-                <span className='icon icon-stats-bars'></span>
-                <span className='icon icon-usb'></span>
-                <span className='icon icon-cog'></span>
-            </div>
-        );
-    }
+const { ipcRenderer } = window.require('electron');
+
+function Navigation(props) {
+    const [debugView, setDebugView] = useState(false);
+    const debug = useRef(0);
+
+    useEffect(() => {
+        debug.current = false;
+        window.addEventListener('keydown', (key) => {
+            if (key.ctrlKey && key.shiftKey && key.code === 'KeyD') {
+                setDebugView(!debug.current);
+                debug.current = !debug.current;
+                console.log('stuff');
+            }
+        });
+
+        return () => {
+
+        };
+    }, []);
+    
+    console.log('Rerender');
+    
+    return(
+        <div className={"Navigation"}>
+            { debugView ? <span className='icon icon-bug'></span> : null }
+            <span className='icon icon-dashboard'></span>
+            <span className='icon icon-stats-bars'></span>
+            <span className='icon icon-usb'></span>
+            <span className='icon icon-cog'></span>
+        </div>
+    );
 };
 
 export default Navigation;
