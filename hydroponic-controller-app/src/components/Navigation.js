928ce8ca2bbc885926629dc5d4fb4b6743587ff9
Charlemagne Wong <frian13@hotmail.com>
2021-02-22 23:30:12 -0600

    Add focus when a navigation button is pressed

--- hydroponic-controller-app/src/components/Navigation.js
+++ hydroponic-controller-app/src/components/Navigation.js
@@ -5,10 +5,40 @@ import './NavigationButton'
 import NavigationButton from './NavigationButton';
 const { ipcRenderer } = window.require('electron');
 
-function Navigation(props) {
+function Navigation({ id=undefined }) {
     const [debugView, setDebugView] = useState(false);
     const debug = useRef(0);
+    const buttonIDs = useRef([]);
 
+    const addButtonFocus = (id) => {
+        if (!(id in buttonIDs.current)) {
+            console.log(`Registering id: ${id}`);
+            buttonIDs.current.push(id);
+            console.log(`button IDs are ${buttonIDs.current}`);
+            return true;
+        }
+        return false;
+    }
+
+    const removeButtonFocus = (id) => {
+        if (id in buttonIDs.current) {
+            for (let i = 0; i < buttonIDs.current.length; ++i) {
+                if (buttonIDs[i] === 'id') {
+                    buttonIDs.current.splice(i, 1);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    const unfocusAll = () => {
+        for (id in buttonIDs.current) {
+            console.log(`Removing ${buttonIDs.current[id]}`);
+            document.getElementById(buttonIDs.current[id]).classList.remove('focus');
+        }
+    }
+    
     useEffect(() => {
         debug.current = false;
         window.addEventListener('keydown', (key) => {
@@ -27,13 +57,12 @@ function Navigation(props) {
     console.log('Rerender');
     
     return(
-        <div className={"Navigation"}>
-            { debugView ? <span className='icon icon-bug'></span> : null }
-            <NavigationButton href='./' icon='icon-usb'/>
-            <span className='icon icon-dashboard'></span>
-            <span className='icon icon-stats-bars'></span>
-            <span className='icon icon-usb'></span>
-            <span className='icon icon-cog'></span>
+        <div id={id} className={"Navigation"}>
+            { debugView ? <NavigationButton id='debug-button' href='#' icon='icon-bug'/> : null }
+            <NavigationButton registerButton={ addButtonFocus } unregisterButton={ removeButtonFocus } unfocusAll={ unfocusAll } id='dashboard-button' href='./' icon='icon-dashboard'/>
+            <NavigationButton registerButton={ addButtonFocus } unregisterButton={ removeButtonFocus } unfocusAll={ unfocusAll } id='data-button' href='#' icon='icon-stats-bars'/>
+            <NavigationButton registerButton={ addButtonFocus } unregisterButton={ removeButtonFocus } unfocusAll={ unfocusAll } id='export-button'href='#' icon='icon-usb'/>
+            <NavigationButton registerButton={ addButtonFocus } unregisterButton={ removeButtonFocus } unfocusAll={ unfocusAll } id='settings-button'href='#' icon='icon-cog'/>
         </div>
     );
 };
--- hydroponic-controller-app/src/components/NavigationButton.js
+++ hydroponic-controller-app/src/components/NavigationButton.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Navigation.css'
 import './NavigationButton.css'
 
-function NavigationButton({href, icon}) {
+function NavigationButton({ id=undefined, href, icon, registerButton=undefined, unregisterButton=undefined, unfocusAll=undefined }) {
     var className = 'icon ' + icon;
     
+    const onClick = () => {
+        if (unfocusAll) unfocusAll();
+        document.getElementById(id)?.classList.add('focus');
+    }
+    
+    useEffect(() => {
+        console.log(`My id is: [${id}] and class ${icon}`)
+        if (registerButton) registerButton(id);
+        
+        return () => {
+            if (unregisterButton) unregisterButton(id);
+        }
+    }, [])
+    
     return (
-        <Link to={href} className='NavigationButton'>
-            <span className={ className } ></span>
+        <Link to={href} className='NavigationButton' onClick={onClick}>
+            <span id={id} className={ className } ></span>
         </Link>
     )
 }
