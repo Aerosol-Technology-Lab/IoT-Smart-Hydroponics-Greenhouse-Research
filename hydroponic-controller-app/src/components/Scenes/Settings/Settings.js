2a79d2fe90a6ece50ed464ee6d8a1cd7b1f9f366
Charlemagne Wong <frian13@hotmail.com>
2021-02-23 01:23:07 -0600

    Add settings this time and color scheme

---
+++ hydroponic-controller-app/src/components/Scenes/Settings/Settings.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Settings = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <h2>IoT Hydroponics Research</h2>
+            <h3>BAEN - Biological & Agricultural Engineering</h3>
+            <h3>Aerosol Lab</h3>
+            <h3>Written by: Charlemagne Wong</h3>
+            <button>Turn Off Rasbperry Pi</button>
+            <button>Restart Rasbperry Pi</button>
+            <button>Update</button>
+        </>
+    )
+}
+
+export default Settings;
