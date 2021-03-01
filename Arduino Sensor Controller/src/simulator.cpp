c29d8ccfe2e4b864d56c47735b4d24be8b201ce1
Charlemagne Wong <frian13@hotmail.com>
2021-03-01 16:22:38 -0600

    Implement changes to simulator

--- Arduino Sensor Controller/src/simulator.cpp
+++ Arduino Sensor Controller/src/simulator.cpp
@@ -1,6 +1,12 @@
 // set this flag in platform.ini under build_flags
 #ifdef SIMULATOR
 
+#include "sensors.h"
+#include <Arduino.h>
+#include "utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 #if defined(DTOSTRF_EXT_LIB) || defined(DUE)
 char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
@@ -11,19 +17,20 @@ char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
 
 #endif
 
-#include "sensors.h"
-#include <Arduino.h>
-#include "utils.h"
-#include <stdlib.h>
-
 void Sensors::init() {
 randomSeed(analogRead(0));
 }
 
-void Sensors::temperature(const char *buffer, size_t buffer_size) {
+void Sensors::temperature(const char *buffer, size_t buffer_size, bool unused) {
     char next[8];
     Utils::nextWord(buffer, 5, 8, next, 8);
 }
+
+size_t temperature(int sensorIdx, char *buffer) {
+    sprintf(buffer, "TMP:0 %.2f TMP:1 %.2f TMP:2 %.2f", random(5000, 10000) * 0.01, random(5000, 10000) * 0.01, random(5000, 10000) * 0.01);
+    return strlen(buffer);
+}
+
 
 void Sensors::ph(const char *buffer, size_t buffer_size) {
     float rand = random(0,1401) * 0.01;
