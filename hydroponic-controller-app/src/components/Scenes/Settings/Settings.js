ae51943470720ee544ca2b26fe22d2309a5825ee
Charlemagne Wong <frian13@hotmail.com>
2021-06-05 14:43:55 -0500

    Changes to settings page

--- hydroponic-controller-app/src/components/Scenes/Settings/Settings.js
+++ hydroponic-controller-app/src/components/Scenes/Settings/Settings.js
@@ -1,35 +1,17 @@
 import React, { useEffect } from 'react';
-import './Settings.css';
-const { ipcRenderer } = window.require('electron');
+import { Route } from 'react-router';
+import SettingsPage from './SettingsPage';
+import Update from './Update';
 
 const Settings = () => {
 
-    const onClickShutdown = () => {
-        ipcRenderer.invoke('app-shutdown');
-    }
-    
     useEffect(() => {
-        window.setNavName?.('Settings');
-        
-        return () => {
-            // window.removeNavName?.();
-        }
     }, [])
     
     return (
-        <div className='SettingsScene'>
-            <h1>About</h1>
-            <h2>IoT Hydroponics Research</h2>
-            <h3>BAEN - Biological & Agricultural Engineering</h3>
-            <h3>Aerosol Lab</h3>
-            <h3>Written by: Charlemagne Wong</h3>
-            <div className='options'>
-                <button>Turn Off Rasbperry Pi</button>
-                <button>Restart Rasbperry Pi</button>
-                <button>Update</button>
-                <button onClick={ onClickShutdown }>Shutdown</button>
-                sfs
-            </div>
+        <div>
+            <Route path='/update' exact component={Update} />
+            <Route path='/settings' exact component={SettingsPage} />
         </div>
     )
 }
