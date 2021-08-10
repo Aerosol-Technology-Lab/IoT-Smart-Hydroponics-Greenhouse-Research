cd65367abb01e6a51660b6feadd4e7fb1e768e68
Charlemagne Wong <frian13@hotmail.com>
2021-08-10 00:03:53 -0500

    Add new installer and script manager

---
+++ hydroponic-controller-app/core2.py
@@ -0,0 +1,110 @@
+import sys
+import subprocess
+import os
+import tempfile
+import pwd
+from datetime import date, datetime
+import requests
+import json
+import re
+from pathlib import Path
+
+REPO_URL = 'https://raw.githubusercontent.com/cmasterx/IoT-Smart-Hydroponics-Greenhouse-Research/{branch}/{project}/{path}'
+PROJECT_NAME = 'hydroponic-controller-app'
+
+TARGET_BRANCH = 'development'
+
+def request_repo_get(path:str, branch:str='release', project:str=PROJECT_NAME, repo_url:str=REPO_URL) -> requests.Response:
+    return requests.get(repo_url.format(project=project, branch=branch, path=path))
+
+def get_app_version(version):
+    nums = re.sub("[^0-9]", " ", version).split(' ')
+    values = len(nums)
+    res = []
+    for s in values:
+        try:
+            foo = int(s)
+            res.append(foo)
+        except:
+            pass
+
+    # this function needs to return an array of numbers with at least length of two
+    while len(res) < 2:
+        res.append(0)
+
+    return res
+            
+
+def update(branch=None):
+
+    cfg = None
+    with open('hydro-cfg/config.json', 'r') as f:
+        cfg = json.load(f)
+
+    if branch is None:
+        branch = cfg['source-branch']
+
+    r = requests.get(REPO_URL.format(project=PROJECT_NAME, branch=branch, path='hydro-core/config.json'))
+    repo_info = r.json()
+    r.close()
+    repo_version = get_app_version(repo_info['version'])
+    this_version = get_app_version(cfg['version'])
+
+    if (repo_version[0] > this_version[0] or 
+            (repo_version[0] == this_version[0] and repo_version[1] > this_version[1])):
+        
+        # do update
+        with request_repo_get('hydro-app') as r:
+            r.raise_for_status()
+            with open('hydro-app', 'w') as f:
+                for chunk in r.iter_content(chunk=8192):
+                    f.write(chunk)
+            
+        cfg['version'] = repo_info['version']
+        with open('/hydro-cfg/config.json', 'w') as f:
+            json.dump(cfg, f)
+
+        return True
+    else:
+        # nothing
+        return False
+        
+    
+
+def main(args):
+
+    if not os.path.exists('hydro-cfg/init'):
+        print('Hydroponics Desktop App is not installed. Let\'s fix that! Installing...')
+
+        if not os.path.exists('hydro-cfg') or not os.path.isdir('hydro-cfg'):
+            os.mkdir('hydro-cfg')
+
+        print('-> Downloading a couple files...')
+
+
+        try:
+            r = requests.get(REPO_URL.format(project=PROJECT_NAME, branch='dev/hydroponic-controller-app', path='hydro-core/config.json'))
+            with open('hydro-cfg/config.json', 'w') as f:
+                f.write(r.text())
+        except:
+            print('\Uh oh... I can\'t connect to the internet at the moment. Please check if there is an internet connection.')
+            exit(1)
+
+        update()
+        
+        with request_repo_get('hydro-core/hydro-starter.desktop') as r:
+            with open(os.path.join(Path.home(), '.config', 'autostart', 'hydro-starter.desktop'), 'w') as f:
+                f.write(r.text.format(exec_path=sys.executable, script_path=os.path.realpath(__file__), args='start boot-start'))
+        
+        with open('hydro-cfg/init', 'w') as f:
+            f.write('Installed on {dt}\n', datetime.now().isoformat())
+    
+    pass
+
+if '__main__' in __name__:
+
+    # changes working directory to path of the file
+    script_path = os.path.realpath(__file__)[:-len(os.path.basename(__file__))]
+    os.chdir(script_path)
+
+    main(sys.argv[1:])
