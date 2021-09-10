92b5a6e9f7fbc952eebd253b02a60464710866ac
Charlemagne Wong <frian13@hotmail.com>
2021-09-09 23:48:53 -0500

    Add export to flash drive

--- hydroponic-controller-app/src/components/Scenes/Export/Export.js
+++ hydroponic-controller-app/src/components/Scenes/Export/Export.js
@@ -1,12 +1,67 @@
-import { React, useEffect, useState } from 'react';
-import './Expost.css'
+import { React, useEffect, useState, useRef } from 'react';
+import './Export.css'
 const { ipcRenderer } = window.require('electron');
+const fs = window.require('fs');
+const os = window.require('os');
+const path = window.require('path');
 
 function Export()
 {
+    const ignoreDirectories = [
+        os.userInfo().username,
+        'root',
+        // 'veracrypt2'
+    ];
+    const [directories, setDirectories] = useState([]);
+    const watcher = useRef(null)
+    
+    const saveToUSB = async (file) => {
+        fs.copyFile('./hydro.csv', path.join('/media', file, 'hydro.csv'), (err) => {
+            if (err) console.log('Cannot copy file');
+        })
+    };
+    
+    useEffect(() => {
+
+
+        watcher.current = setInterval(() => {
+            
+            fs.readdir('/media', (err, files) => {
+                if (!err) {
+                    setDirectories(files);
+                }
+              });
+
+        }, 1000);
+          
+        return () => {
+            clearInterval(watcher.current);
+        }
+    }, [])
+    
+    var drives = [];
+    directories.forEach(file => {
+        if (!ignoreDirectories.includes(file)) drives.push(file);
+    })
+    
     return (
-        <div>
-            hello!
+        <div className="Export">
+            <div className="content">
+                { 
+                    drives.length ?
+                    drives.map((file, index) => {
+
+                        return (
+                            <div className="drive" onClick={ () => {saveToUSB(file)} }>
+                                <img className="usb" src="https://static.thenounproject.com/png/250776-200.png" />
+                                <p className="filename">{file}</p>
+                            </div>
+                        )
+                    })
+                    :
+                    'no usb detected'
+                }
+            </div>
         </div>
     )
 }
